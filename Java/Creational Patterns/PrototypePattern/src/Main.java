558efbee1299c2e8ce27c90dbc04acbe6bba1456
AhmetY7 <ahmetylmz95@gmail.com>
2020-09-15 22:21:20 +0300

    Proje Resim

---
+++ Java/Creational Patterns/PrototypePattern/src/Main.java
@@ -0,0 +1,58 @@
+import shapes.Circle;
+import shapes.Rectangle;
+import shapes.Shape;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Prototype is a creational design pattern that lets you copy existing objects without making your code dependent on their classes.
+ *
+ * Use the Prototype pattern when your code shouldn’t depend on the concrete classes of objects that you need to copy.
+ *
+ * Use the pattern when you want to reduce the number of subclasses that only differ in the way they initialize their respective objects.
+ * Somebody could have created these subclasses to be able to create objects with a specific configuration.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        List<Shape> shapes = new ArrayList<>();
+        List<Shape> shapesCopy = new ArrayList<>();
+
+        Circle circle = new Circle();
+        circle.x = 10;
+        circle.y = 20;
+        circle.radius = 15;
+        circle.color = "red";
+        shapes.add(circle);
+
+        Circle anotherCircle = (Circle) circle.clone();
+        shapes.add(anotherCircle);
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.width = 10;
+        rectangle.height = 20;
+        rectangle.color = "blue";
+        shapes.add(rectangle);
+
+        cloneAndCompare(shapes, shapesCopy);
+    }
+    private static void cloneAndCompare(List<Shape> shapes, List<Shape> shapesCopy) {
+        for (Shape shape : shapes) {
+            shapesCopy.add(shape.clone());
+        }
+
+        for (int i = 0; i < shapes.size(); i++) {
+            if (shapes.get(i) != shapesCopy.get(i)) {
+                System.out.println(i + ": Shapes are different objects (yay!)");
+                if (shapes.get(i).equals(shapesCopy.get(i))) {
+                    System.out.println(i + ": And they are identical (yay!)");
+                } else {
+                    System.out.println(i + ": But they are not identical (booo!)");
+                }
+            } else {
+                System.out.println(i + ": Shape objects are the same (booo!)");
+            }
+        }
+    }
+}
